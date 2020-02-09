6dca85e74681eda2ddbc42fd75a8b757092b922c
John Kisor <jkisor@gmail.com>
2020-02-09 12:58:06 -0500

    extract body view model

---
+++ include/body_view_model.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "view_model.h"
+
+struct BodyViewModel : public ViewModel
+{
+  BodyViewModel(Snake &snake, int index)
+  {
+    int SIZE = 64;
+    int SCALE = 4.0f;
+    int FRAME_SIZE = 16.0f;
+
+    Position p = snake.positions[index];
+    Position aheadPos = snake.positions[index-1];
+    Position behindPos = snake.positions[index+1];
+
+    origin = { FRAME_SIZE/2, FRAME_SIZE/2 };
+    position = { (p.x * SIZE) + SIZE/2, (p.y * SIZE) + SIZE/2 };
+    scale = { SCALE, SCALE };
+
+    Direction a = { aheadPos.x - p.x, aheadPos.y - p.y };
+    Direction b = { p.x - behindPos.x, p.y - behindPos.y };
+
+    if(a == b)
+    {
+      if(a == RIGHT)
+        rotation = { 0 };
+      else if(a == LEFT)
+        rotation = { 180 };
+      else if(a == UP)
+        rotation = { 90 };
+      else if(a == DOWN)
+        rotation = {-90};
+
+      rectangle = { FRAME_SIZE * 1, 0, FRAME_SIZE, FRAME_SIZE };
+    }
+    else if( (a == LEFT && b == UP)
+            || (a == DOWN && b == RIGHT)
+    )
+    {
+      rotation = {0};
+      rectangle = { FRAME_SIZE * 4, 0, FRAME_SIZE, FRAME_SIZE };
+    }
+    else if( (a == LEFT && b == DOWN)
+            || (a == UP && b == RIGHT))
+    {
+
+      rotation = {90};
+      rectangle = { FRAME_SIZE * 4, 0, FRAME_SIZE, FRAME_SIZE};
+    }
+    else if( (a == RIGHT && b == DOWN)
+            || (a == UP && b == LEFT))
+    {
+
+      rotation = {180};
+      rectangle = { FRAME_SIZE * 4, 0, FRAME_SIZE, FRAME_SIZE };
+    }
+    else if( (a == DOWN && b == LEFT)
+            || (a == RIGHT && b == UP))
+    {
+      rotation = {270};
+      rectangle = { FRAME_SIZE * 4, 0, FRAME_SIZE, FRAME_SIZE };
+    }
+    else // ERROR
+    {
+      rotation = {0};
+      rectangle = { FRAME_SIZE * 3, 0, FRAME_SIZE, FRAME_SIZE };
+    }
+
+  }
+};
--- src/snake_view.cpp
+++ src/snake_view.cpp
@@ -2,6 +2,7 @@
 
 #include "head_view_model.h"
 #include "arrow_view_model.h"
+#include "body_view_model.h"
 
 SnakeView::SnakeView()
 {
@@ -21,8 +22,7 @@ std::vector<sf::Sprite> SnakeView::drawables(Snake &snake)
   output.insert(output.end(), body_sprites.begin(), body_sprites.end() );
 
   // Head
-  sf::Sprite sprite = buildSprite(HeadViewModel(snake));
-  output.push_back(sprite);
+  output.push_back(buildSprite(HeadViewModel(snake)));
 
   // Arrow
   if(!snake.dead)
@@ -37,80 +37,11 @@ std::vector<sf::Sprite> SnakeView::drawables(Snake &snake)
 
 std::vector<sf::Sprite> SnakeView::bodySprites(Snake &snake)
 {
-  int SIZE = 64;
-  int SCALE = 4.0f;
-  int FRAME_SIZE = 16.0f;
-
   std::vector<sf::Sprite> results;
 
   for(int i = 1; i < snake.positions.size()-1; i++)
   {
-    Position p = snake.positions[i];
-    Position aheadPos = snake.positions[i-1];
-    Position behindPos = snake.positions[i+1];
-
-    sf::Sprite sprite;
-
-    sprite.setTexture(texture);
-    sprite.setOrigin(FRAME_SIZE/2, FRAME_SIZE/2);
-    sprite.setPosition((p.x * SIZE) + SIZE/2, (p.y * SIZE) + SIZE/2);
-    sprite.setScale(SCALE, SCALE);
-
-    Direction a = { aheadPos.x - p.x, aheadPos.y - p.y };
-    Direction b = { p.x - behindPos.x, p.y - behindPos.y };
-
-    float rotation;
-    sf::IntRect rect;
-
-    if(a == b)
-    {
-      if(a == RIGHT)
-        rotation = 0.0f;
-      else if(a == LEFT)
-        rotation = 180.0f;
-      else if(a == UP)
-        rotation = 90.0f;
-      else if(a == DOWN)
-        rotation = -90.0f;
-
-      rect = sf::IntRect(FRAME_SIZE * 1, 0, FRAME_SIZE, FRAME_SIZE);
-    }
-    else if( (a == LEFT && b == UP)
-            || (a == DOWN && b == RIGHT)
-    )
-    {
-      rotation = 0;
-      rect = sf::IntRect(FRAME_SIZE * 4, 0, FRAME_SIZE, FRAME_SIZE);
-    }
-    else if( (a == LEFT && b == DOWN)
-            || (a == UP && b == RIGHT))
-    {
-
-      rotation = 90;
-      rect = sf::IntRect(FRAME_SIZE * 4, 0, FRAME_SIZE, FRAME_SIZE);
-    }
-    else if( (a == RIGHT && b == DOWN)
-            || (a == UP && b == LEFT))
-    {
-
-      rotation = 180;
-      rect = sf::IntRect(FRAME_SIZE * 4, 0, FRAME_SIZE, FRAME_SIZE);
-    }
-    else if( (a == DOWN && b == LEFT)
-            || (a == RIGHT && b == UP))
-    {
-      rotation = 270;
-      rect = sf::IntRect(FRAME_SIZE * 4, 0, FRAME_SIZE, FRAME_SIZE);
-    }
-    else // ERROR
-    {
-      rotation = 0;
-      rect = sf::IntRect(FRAME_SIZE * 3, 0, FRAME_SIZE, FRAME_SIZE);
-    }
-
-    sprite.setRotation(rotation);
-    sprite.setTextureRect(rect);
-
+    sf::Sprite sprite = buildSprite(BodyViewModel(snake, i));
     results.push_back(sprite);
   }
 
