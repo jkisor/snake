e6fdff796aeb806291e55cfc59d175612e2ee71a
John Kisor <jkisor@gmail.com>
2020-02-15 11:02:46 -0500

    don'y spawn pickup on top of snake

--- src/spawn_pickup.cpp
+++ src/spawn_pickup.cpp
@@ -1,4 +1,6 @@
 #include "spawn_pickup.h"
+#include <algorithm>
+#include <vector>
 
 SpawnPickup::SpawnPickup(State &state)
 {
@@ -7,7 +9,22 @@ SpawnPickup::SpawnPickup(State &state)
 
 void SpawnPickup::call()
 {
+
+  std::vector<Position> positions;
+
+  for(int y = 0; y < state->bounds.height; y++)
+  {
+    for(int x = 0; x < state->bounds.width; x++)
+    {
+      std::vector<Position> snakePos = state->snake.positions;
+
+      if (std::find(snakePos.begin(), snakePos.end(), Position({x,y})) == snakePos.end())
+        positions.push_back({x,y});
+
+    }
+  };
+
   Pickup nextPickup;
-  nextPickup.position = { rand() % state->bounds.width, rand() % state->bounds.height };
+  nextPickup.position = positions[rand() % positions.size()];
   state->pickup = nextPickup;
 }
